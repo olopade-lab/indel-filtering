118a25ea1850194c839ee4c6da53f48259fee000
Anna Elizabeth Woodard <awoodard@nd.edu>
2019-11-06 10:43:46 -0600

    Update read_vcf
    Now it handles both compressed and uncompressed files.

--- utils.py
+++ utils.py
@@ -2,11 +2,8 @@ import gzip
 import io
 import pandas as pd
 
-def read_vcf(path):
-    with gzip.open(path, 'r') as f:
-        lines = [l for l in f if not l.startswith(b'##')]
-    return pd.read_csv(
-        io.BytesIO(b''.join(lines)),
+def read_vcf(
+        path,
         dtype={
             '#CHROM': str,
             'POS': int,
@@ -19,6 +16,21 @@ def read_vcf(path):
             'FORMAT': str,
             'NORMAL': str,
             'TUMOUR': str
-        },
-        sep='\t'
-    ).rename(columns={'#CHROM': 'CHROM'})
+        }
+    ):
+    if path.endswith('.gz'):
+        with gzip.open(path, 'r') as f:
+            lines = [l for l in f if not l.startswith(b'##')]
+        return pd.read_csv(
+            io.BytesIO(b''.join(lines)),
+            dtype=dtype,
+            sep='\t'
+        ).rename(columns={'#CHROM': 'CHROM'})
+    else:
+        with open(path, 'r') as f:
+            lines = [l for l in f if not l.startswith('##')]
+        return pd.read_csv(
+            ''.join(lines),
+            dtype=dtype,
+            sep='\t'
+        ).rename(columns={'#CHROM': 'CHROM'})
